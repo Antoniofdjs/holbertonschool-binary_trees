05a614aac92b9590605e91aaf7181152d5867560
osvaldo antompietri <antompietriosvaldo@gmail.com>
2024-01-10 17:40:32 -0400

    Official file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- *binary_tree_insert_right
+ *binary_tree_insert_right - function that inserts a node as the right-child of another node
  *@parent: is a pointer to the node to insert the left-child in
  *@value: is the value to store in the new node
  */
