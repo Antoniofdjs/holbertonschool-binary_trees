8c080404e53d8872d26b8f1806595ee0e5c5fcec
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-10 12:44:54 -0400

    Added the print tree prototype

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -21,6 +22,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+void binary_tree_print(const binary_tree_t *);
 #endif /* BINARY_TREES_H */
