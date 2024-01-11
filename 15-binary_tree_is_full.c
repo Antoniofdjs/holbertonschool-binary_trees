11cc3b59b4bbdc36f7f193f486593581f7340195
ojvl1 <carlosantompietri175@gmail.com>
2024-01-11 10:39:17 -0400

    fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,16 +10,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
     if (tree == NULL) 
     {
-        return 1;
+        return (0);
     }
 
     if (tree->left != NULL && tree->right != NULL) 
     {
-        return 0;
+        return (0);
     }
     if (tree->left != NULL || tree->right != NULL) 
     {
-        return 1;
+        return (1);
     }
-    return 1;
 }
