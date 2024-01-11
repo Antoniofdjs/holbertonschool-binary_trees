a46886535c1c1cf589b27ab22b87cc0c34b80c35
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-11 12:53:01 -0400

    fixed after deleting som variables by mistake

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,37 +3,37 @@
 /**
  * binary_tree_is_full - Check if a binary tree is full.
  * @tree: A pointer to the root node of the tree to check.
-
+ *
  * Return: 1 if the tree is full, 0 otherwise.
  */
-
+int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-
+		return (1);
 
 	if (tree->left != NULL && tree->right != NULL)
-
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	return (0);
-
+}
 
 /**
-
+ * binary_tree_height - binary_tree_height
  *
  * @tree: tree
-
+ *
  * Return: height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
+	size_t left_height, right_height;
 
 	if (tree == NULL)
-
+		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	return ((left_height > right_height ? left_height : right_height) + 1);
