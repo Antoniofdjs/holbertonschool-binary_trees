403234dcc8d7e9c0a22fdba799f46be3ebaa6916
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-11 13:00:06 -0400

    check if final node both are nudes the left and right

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -56,6 +56,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height == right_height && f)
-		return (1);
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
 	return (0);
 }
