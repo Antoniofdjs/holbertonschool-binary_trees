09f95a39062b54f690b9c72cc7c39b9a04cdbeea
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-11 00:34:02 -0400

    Helper was changed but why not working wiht original one from 9?

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Gives height of node
- * @tree: Node of binary tree
- * Return: Height of the tree, or -1 if tree is NULL
+ * binary_tree_height - binary_tree_height
+ *
+ * @tree: tree
+ *
+ * Return: height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -12,51 +14,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (left_height);
-		return (left_height + 1);
-	}
-	else
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (right_height);
-		return (right_height + 1);
-	}
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
- * binary_tree_balance - Gives balance
- * @tree: Node of binary tree
- * Return: Height of the tree, or -1 if tree is NULL
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure.
+ *
+ * Return: The balance factor of the binary tree.
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	int left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
-
-
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (left_height - right_height);
-		return (left_height - right_height);
-	}
-	else
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (left_height - right_height);
-		return (left_height - (right_height + 1));
-	}
+	return (left_height - right_height);
 }
