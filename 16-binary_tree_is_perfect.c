cf2241d2fb5902ebaed68c012b3cf9b9e712a47e
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-11 12:40:13 -0400

    added to check if tree is also full for perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_full - Check if a binary tree is full.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is full, 0 otherwise.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
+
+/**
  * binary_tree_height - binary_tree_height
  *
  * @tree: tree
@@ -28,13 +48,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height, right_height, f = 0;
 
 	if (tree == NULL)
 		return (0);
+	f = binary_tree_is_full(tree);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	if ((left_height - right_height) == 0)
+	if ((left_height == right_height) && (f == 1))
 		return (1);
 	return (0);
 }
